9f294018c2143d473c95014e4017782dd3e72026
marciomarinho <marciomarinho@gmail.com>
2016-03-13 12:17:12 +1100

    Code review - Adding End-to-End or Controller test.

--- LibraryProjectt/src/test/java/com/library/LibraryApplicationTests.java
+++ LibraryProjectt/src/test/java/com/library/LibraryApplicationTests.java
@@ -1,18 +1,46 @@
 package com.library;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = LibraryApplication.class)
 @WebAppConfiguration
 public class LibraryApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
     }
 
+    //TODO: Take a look at --> http://blog.lanyonm.org/spring-grabbag/testapidocs/src-html/org/lanyonm/grabbag/web/controller/CookbookControllerTest.html
+    @Test
+    public void testHome() throws Exception {
+
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("welcomeview"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attributeExists("readers"))
+                .andExpect(model().attributeExists("bookLeadings"));
+
+    }
 }
